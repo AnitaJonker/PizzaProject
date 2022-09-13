043a051b5493cc048fa96d173eb5ac7d1443bc98
Anita <anitaroneljonker@gmail.com>
2022-09-13 11:48:02 +0200

    new pizza

--- pizza/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from './App';
-import './index.css'
-
-// eslint-disable-next-line no-unused-expressions
-ReactDOM.render(<App />, document.getElementById('root'))
